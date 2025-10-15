ce832a7f1f91ceff7e12077f396016ea8df0917d
Sandro sulava <sandrosulava2@gmail.com>
2025-10-15 19:49:09 +0400

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import FutureTechnology from "./components/FutureTechnology/FutureTechnology";
+
 const App = () => {
   return (
     <div>
-      <h1>Hello world</h1>
+      <FutureTechnology />
     </div>
   );
 };
