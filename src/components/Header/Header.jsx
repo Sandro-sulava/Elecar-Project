3a95e6af17e3a396217c5486219257e14dc40d19
Mindia Arabuli <mindia.arabuli@iliauni.edu.ge>
2025-10-24 20:08:56 +0400

    Add logo SVG and update Header component
    Added Logo.svg asset and updated Header to use the new logo path. Adjusted Header CSS to set a max-width for the container. Replaced StrictMode with BrowserRouter in main.jsx to enable routing.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,21 @@
 import styles from "./Header.module.css";
-import logo from "../assets/logo.svg";
 import Navbar from "../Navbar/Navbar";
 
 const Header = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.HeaderBox}>
-        <a href="/">
-          <img src={logo} alt={logo} className={styles.Headerlogo} />
-        </a>
-      </div>
-      <Navbar />
-      <div className={styles.burger}>
-        <span></span>
-        <span></span>
-        <span></span>
+    <div className={styles.header}>
+      <div className={styles.container}>
+        <div className={styles.HeaderBox}>
+          <a href="/">
+            <img src="/logo.svg" alt="Logo" className={styles.Headerlogo} />
+          </a>
+        </div>
+        <Navbar />
+        <div className={styles.burger}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
       </div>
     </div>
   );
