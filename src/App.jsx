68b5816c1bc4f87ec91de936f732a4ce8c29b05f
Mindia Arabuli <mindia.arabuli@iliauni.edu.ge>
2025-10-24 19:52:25 +0400

    Add Header and Navbar components with routing support
    Introduced Header and Navbar components with associated CSS modules for layout and styling. Integrated react-router-dom for navigation and updated App.jsx to include the new Header. Updated dependencies in package.json to include react-router-dom.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import FutureTechnology from "./components/FutureTechnology/FutureTechnology";
-import Footer from "./components/Footer/Footer"
-
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
 
 const App = () => {
   return (
     <div>
+      <Header />
       <FutureTechnology />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+import styles from "./Header.module.css";
+import logo from "../assets/logo.svg";
+import Navbar from "../Navbar/Navbar";
+
+const Header = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.HeaderBox}>
+        <a href="/">
+          <img src={logo} alt={logo} className={styles.Headerlogo} />
+        </a>
+      </div>
+      <Navbar />
+      <div className={styles.burger}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
