e727e970f643a5a977789fc18696a7198616322c
Mb-03 <baratashvilimisho@gmail.com>
2025-10-30 17:40:59 +0400

    feature/filter
    added filtration

--- src/components/LuxuryCars/LuxuryCars.jsx
+++ src/components/LuxuryCars/LuxuryCars.jsx
@@ -1,32 +1,100 @@
 import styles from "./LuxuryCars.module.css";
-import { cardInfo } from "./CardInfo";
+import { keepPreviousData, useQuery } from "@tanstack/react-query";
+import { useRef, useState } from "react";
+export const API_BASE =
+  import.meta.env.VITE_API_URL || "http://localhost:4000/api";
 
 const LuxuryCars = () => {
+  const [brand, setBrand] = useState(null);
+
+  const handleResponse = async (res) => {
+    if (!res.ok) {
+      const text = await res.text().catch(() => "Unknown error");
+      throw new Error(text);
+    }
+    return res.json();
+  };
+
+  const getLuxuryCars = async (brand) => {
+    if (brand) {
+      const res = await fetch(`${API_BASE}/luxuryCar?brand=${brand}`);
+      return handleResponse(res);
+    } else {
+      const res = await fetch(`${API_BASE}/luxuryCar`);
+      return handleResponse(res);
+    }
+  };
+
+  const { data, isPending, error } = useQuery({
+    queryKey: ["luxuryCars", brand],
+    queryFn: () => getLuxuryCars(brand),
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.luxuryCars}>
         <h2>Featurd Luxury Cars</h2>
         <div className={styles.buttonContainer}>
-          <button>All</button>
-          <button>
+          <button
+            style={{
+              backgroundColor: !brand && "#4177dc",
+            }}
+            onClick={() => {
+              setBrand(null);
+            }}
+          >
+            All
+          </button>
+          <button
+            onClick={() => {
+              {
+                brand === "Tesla" ? setBrand(null) : setBrand("Tesla");
+              }
+            }}
+            style={{
+              backgroundColor: brand === "Tesla" && "#4177dc",
+            }}
+          >
             <img src="tesla-logo.svg" alt="tesla logo" />
           </button>
-          <button>
+          <button
+            style={{
+              backgroundColor: brand === "Audi" && "#4177dc",
+            }}
+            onClick={() => {
+              {
+                brand === "Audi" ? setBrand(null) : setBrand("Audi");
+              }
+            }}
+          >
             <img src="audi-logo.svg" alt="audi logo" />
           </button>
-          <button>
+          <button
+            style={{
+              backgroundColor: brand === "Porsche" && "#4177dc",
+            }}
+            onClick={() => {
+              {
+                brand === "Porsche" ? setBrand(null) : setBrand("Porsche");
+              }
+            }}
+          >
             <img src="porsche-logo.svg" alt="porsche logo" />
           </button>
         </div>
         <div className={styles.cardContainerContainer}>
           <div className={styles.cardContainer}>
-            {cardInfo.map((info) => (
+            {isPending && <div className={styles.loader} />}
+            {error?.message && (
+              <p className={styles.error}>Failed to Load Car</p>
+            )}
+            {data?.map((info) => (
               <div className={styles.card}>
-                <h3>{info.make}</h3>
+                <h3>{info.brand}</h3>
                 <p>{info.model}</p>
                 <img src={info.img} alt="car" />
                 <div className={styles.cardBackground} />
-                <h3>{info.price}</h3>
+                <h3>${info.price}</h3>
                 <button className={styles.cardBuyBtn}>
                   <img
                     className={styles.buttonImg}
