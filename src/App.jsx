f4d54af282203a4d81da647e0648f28dc4183457
Sandro sulava <sandrosulava2@gmail.com>
2025-11-06 21:02:32 +0400

    Add new SVG assets and update feature components
    Added Map.svg and featured-car.svg to the public directory. Updated App.jsx, Feature.jsx, and Feature.module.css to integrate new SVG assets and adjust feature component styles and logic.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import FutureTechnology from "./components/FutureTechnology/FutureTechnology";
-import Footer from "./components/Footer/Footer"
-
+import Footer from "./components/Footer/Footer";
+import Feature from "./components/feature/Feature";
 
 const App = () => {
   return (
     <div>
       <FutureTechnology />
+      <Feature />
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -1,7 +1,33 @@
 import "../Styles/global.css";
+import styles from "./Feature.module.css";
 
 const Feature = () => {
-  return <div className={Styles.features}>123123</div>;
+  return (
+    <div className={styles["features"]}>
+      <div>
+        <div>
+          <img src="/Map.svg" alt="map" className={styles["location-map"]} />
+        </div>
+        <div className={styles["car-image"]}>
+          <img src="/featured-car.svg" alt="car" />
+        </div>
+        <div>
+          <div>
+            <p>800v</p>
+            <span>Turbo Chargin</span>
+          </div>
+          <div>
+            <p>350</p>
+            <span>Km Range</span>
+          </div>
+          <div>
+            <p>480</p>
+            <span>Km Travel</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Feature;
