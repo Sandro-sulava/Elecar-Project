7c96e0c07684078bc2facc5a50c2ada57bb1a2e9
Sandro sulava <sandrosulava2@gmail.com>
2025-11-07 19:57:42 +0400

    Refactor car image and popup styling in Feature component
    Moved car image styling to a new .porsche-car-img class and added unique classes for each car info popup for more precise positioning. Updated JSX structure to remove unnecessary divs and apply new class names, improving maintainability and layout control.

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -5,22 +5,25 @@ const Feature = () => {
   return (
     <div className={styles["features"]}>
       <div>
-        <div>
-          <img src="/Map.svg" alt="map" className={styles["location-map"]} />
-        </div>
+        <img src="/Map.svg" alt="map" className={styles["location-map"]} />
+
         <div className={styles["car-info-section"]}>
           <div className={styles["car-image"]}>
-            <img src="/featured-car.svg" alt="car" />
+            <img
+              src="/featured-car.svg"
+              alt="car"
+              className={styles["porsche-car-img"]}
+            />
             <div>
-              <div className={styles["car-info-popup"]}>
+              <div className={styles["car-info-popup car-info-popup1"]}>
                 <h3 className={styles["car-info-h3"]}>800v</h3>
                 <p className={styles["car-info-p"]}>Turbo Chargin</p>
               </div>
-              <div className={styles["car-info-popup"]}>
+              <div className={styles["car-info-popup car-info-popup2"]}>
                 <h3 className={styles["car-info-h3"]}>350</h3>
                 <p className={styles["car-info-p"]}>Km Range</p>
               </div>
-              <div className={styles["car-info-popup"]}>
+              <div className={styles["car-info-popup car-info-popup3"]}>
                 <h3 className={styles["car-info-h3"]}>480</h3>
                 <p className={styles["car-info-p"]}>Km Travel</p>
               </div>
